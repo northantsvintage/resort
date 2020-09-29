cf43304560e48e158ad685c725bd0c9e0d09d8e0
northantsvintage <northantsvintage@gmail.com>
2020-09-29 16:50:21 +0100

    router added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Rooms from "./pages/Rooms";
 import Room from "./pages/Room";
 import Error from "./pages/Error";
 
+import { Route, Swith } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <Room />
-      <Error />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={Room} />
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Homr</div>;
+  return <div>Home</div>;
 }
